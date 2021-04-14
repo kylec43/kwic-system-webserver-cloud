4e63d5f83f7ccc5c47c8f44e76c9c842925f31c0
kylec43 <kylecurry43@gmail.com>
2021-04-14 05:44:41 -0500

    commit

--- DatabaseController.py
+++ DatabaseController.py
@@ -7,10 +7,10 @@ class DatabaseController:
 		pass
 
 	def upload(self, originalUrlKeywords, kwicUrlKeywords, noiseWords):
+
 		success = True
+		error = None
 		try:
-
-
 			SERVER_IP = 'localhost'  
 			PORT = 12001
 
@@ -40,8 +40,6 @@ class DatabaseController:
 				raise Exception(Constants.SERVER_RESPONSE_UPLOAD_FAILURE)
 			client_socket.sendall(noiseWords.encode('utf-8'))
 
-
-
 			#Recieve a response from the Server
 			client_socket.settimeout(60)
 			response = client_socket.recv(100)
@@ -51,12 +49,58 @@ class DatabaseController:
 
 		except Exception as e:
 			success = False
-			print(e)
+			error = e
 		finally:
 			client_socket.close()
-
 			if not success:
-				raise Exception(Constants.SERVER_RESPONSE_UPLOAD_FAILURE)
+				raise error
+
+
+
 
 	def getUrlsKeywords(self, keywords):
-		pass
+		queryResults = ""
+		success = True
+		error = None
+		try:
+
+
+			keywords = keywords.encode('utf-8')
+
+
+			SERVER_IP = 'localhost'  
+			PORT = 12001
+
+			#Create Client Socket
+			client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			print('created')
+			client_socket.settimeout(10)
+
+			#Connect to Server Socket
+			client_socket.connect((SERVER_IP, PORT))
+			client_socket.settimeout(10)
+
+			#Send Message to Server
+			client_socket.sendall(Constants.REQUEST_TYPE_QUERY)
+			queryResults = client_socket.recv(100)
+			client_socket.sendall(keywords)
+
+			#Recieve a response from the Server
+			client_socket.settimeout(120)
+			queryResults = client_socket.recv(500000)
+			print('Received:')
+			if queryResults == Constants.SERVER_RESPONSE_QUERY_FAILURE:
+				raise Exception('SEARCH FAILURE')
+
+		except Exception as e:
+			error = e
+			success = False
+			print(e)
+		finally:
+
+			client_socket.close()
+
+			if success:
+				return queryResults
+			else:
+				raise error
